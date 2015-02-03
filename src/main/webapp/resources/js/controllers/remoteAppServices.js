130ca7026c77684f1fc7e1c8f251f3dd1c7bcbda
erico <ericomercial@yahoo.com.br>
2015-02-02 22:29:47 -0200

    organizacao, implementacao inicial do ng-route

--- src/main/webapp/resources/js/controllers/remoteAppServices.js
+++ src/main/webapp/resources/js/controllers/remoteAppServices.js
@@ -1,4 +1,24 @@
-var remoteAppServices = angular.module('remoteAppServices', ['ras.traducaoMensagens']);
+var remoteAppServices = angular.module('remoteAppServices', [
+    'ras.traducaoMensagens',
+    'ngRoute'
+]);
+
+remoteAppServices.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/cadastro', {
+        templateUrl: 'partials/cadastro.html'
+      }).
+      when('/home', {
+        templateUrl: 'partials/home.html'
+      }).
+      when('/login', {
+        templateUrl: 'partials/login.html'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }]);
 
 remoteAppServices.controller('RemoteAppServicesController', function ($scope) {
   $scope.nome = 'erico';
